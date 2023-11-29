934b3df3f093cc9256f738b38010450b1e9d3654
vvwvvwwvvw <wjddnr3304@gmail.com>
2023-11-29 15:39:17 +0900

    first commit

---
+++ src/a/fist.java
@@ -0,0 +1,15 @@
+//현재 클래스가 (자바파일) 소속된 패키지의 이름
+package a;
+
+// 현재 클래스의 이름
+public class fist { // first class 블록의 시작
+
+// main 매서드(프로그램의 시작 지점)	
+	public static void main(String[] args) {//main 메서드 블록의 시작
+		// 실행할 내용이 작성되는 공간
+		//텍스트 또는 변수값을 출력할수 있는 println()메서드
+		System.out.println("자바야 앞으로 친하게 지내자..");
+
+	}// main 메서드 블록의 
+
+}// first class 블록의 끝
---
+++ src/b/second.java
@@ -0,0 +1,11 @@
+package b;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!!");
+
+	}
+
+}
